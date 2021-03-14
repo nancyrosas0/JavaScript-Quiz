e73f1b21d045aaf0944f90b4e17c72498cc27748
Nancy <nancyrosas0@gmail.com>
2021-03-14 15:25:49 -0500

    added logic, buttons, and functions

--- assets/logic.js
+++ assets/logic.js
@@ -1,10 +1,12 @@
 //var to keep track of 
-var time = 120;
+var time = 100;
 var currentQuestionIndex = 0;
 var timer;
+var score = 100;
 
 //vars for DOM elements
-var introEl = document.querySelector("intro");
+var introEl = document.querySelector(".intro");
+var feedbackEl = document.querySelector(".explain")
 var questionsEl = document.getElementById("questions");
 var startBtn = document.getElementById("start"); 
 var timerEl = document.getElementById("time");
@@ -13,13 +15,19 @@ var submitBtn = document.getElementById("submit");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 var titleEl = document.getElementById("question-title");
+var homeEl = document.getElementById("landing-page")
+var endEl = document.getElementById("end-screen");
+var highscoresEl = document.getElementById("highscores")
+var highscoresBtn = document.getElementById("highscoresBtn")
+var returnBtn = document.getElementById("returnBtn")
 
 
 function startQuiz() {
     
-//introEl.classList.add("hide")
+//document.getElementById.removeAttribute("class");
 //this remove, REMOVES the class and pops up a NEW class! This will make a new div pop up behind it. 
-        
+    homeEl.classList.add("hide")
+    questionsEl.classList.remove("hide")    
     timer = setInterval(clockTick, 1000)
 
     timerEl.textContent = time;
@@ -33,11 +41,11 @@ function getQuestion(){
     //var titleEl = document.getElementById("question-title");
     titleEl.textContent = currentQuestion.title;
     choicesEl.textContent = "";
-    currentQuestion.choices.forEach(function(choice, i){
+    currentQuestion.choices.forEach(function(choice){
         var c = document.createElement("button");
         c.setAttribute('class', 'choice')
         c.setAttribute('value', choice);
-        c.textContent = i + 1 + "." + choice;
+        c.textContent = choice;
         c.onclick = choiceClick;
         //append the button we just made to the entire choices div
         choicesEl.append(c);
@@ -49,23 +57,38 @@ function choiceClick(){
     var correctAnswer = questions[currentQuestionIndex].answers;
     var buttonValue = this.value
     if (buttonValue === correctAnswer){
+        feedbackEl.textContent = "Correct!"
         //no deductions on the timer and move forward
     }else {
-        time-= 5;
+        time = time-5;
+        score = score-20;
+        feedbackEl.textContent = "Incorrect"
         //the timer deducts 5 secs and move forward
     }
-    //? buttonValue === correctAnswer
-//check if correct answer then it's right, if not, it's wrong, then deduct 10 secs from the timer
-//
-    console.log(buttonValue)
+    feedbackEl.classList.remove("hide")
+    setTimeout(function() {
+      feedbackEl.classList.add("hide")
+    }, 2000);
+
+    nextQuestion()
+}
+
+function nextQuestion(){
+    currentQuestionIndex = currentQuestionIndex +1
+    if (questions[currentQuestionIndex] === undefined) {
+
+        quizEnd()
+    }else {
+        getQuestion()
+    }
 }
 
 function quizEnd() {
     clearInterval(timer);
-    var endEl = document.getElementById("end-screen");
-    endEl.removeAttribute("class");
+    endEl.classList.remove("hide")
+    questionsEl.classList.add("hide")  
     var finalScoreEl = document.getElementById("final-score");
-    finalScoreEl.textContent = time;
+    finalScoreEl.textContent = score;
 }
 
 function clockTick(){
@@ -76,8 +99,40 @@ function clockTick(){
     }
 }
 
-function saveHighScore() {
+function saveHighscore() {
+    var initials = initialsEl.value.trim()
+    console.log(initials)
+    var highscores = []
+    var newUser = {
+        score: score,
+        initials: initials
+    }
+    highscores.push(newUser)
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+viewHighscores()
+}
 
+function viewHighscores() {
+    endEl.classList.add("hide")
+    homeEl.classList.add("hide")
+    questionsEl.classList.add("hide")
+    highscoresEl.classList.remove("hide")    
+}
+
+function viewHome() {
+    highscoresEl.classList.add("hide")
+    homeEl.classList.remove("hide")
 }
 
 startBtn.onclick = startQuiz;
+submitBtn.addEventListener("click", saveHighscore);
+highscoresBtn.addEventListener("click", viewHighscores);
+returnBtn.addEventListener("click", viewHome);
+
+//PUll out timer and view Highscores into a Navbar so they don't hide <nav>
+//Style with css
+//use getitem local storage to put items into an array. Get what exists and put them into the highscores array []
+//**remember, nr should not have been replaced by tt 
+//view highscores page - need to get the item from local storage and use it to render their scores & initials on the page
+//need to fix the return home button
+//when return button is clicked, reset timer and score-- this may fix the return home button (check if the timer is causing this misfire)
--- assets/questions.js
+++ assets/questions.js
@@ -1,12 +1,24 @@
 var questions = [
     {
         title: "Commonly used data types do not include",
-        choices: ["boolean", "number", "decimal", "string"],
-        answers: "decimal"
+        choices: ["Boolean", "Number", "Decimal", "String"],
+        answers: "Decimal"
     }, {
-        title: "What does",
-        choices: ["no", ],
-        answers: "Yes"
-    },
+        title: "The condition of an if/else statement is wrapped in ____",
+        choices: ["Periods", "Exclamation Points", "Ampersands", "Parenthesis"],
+        answers: "Parentheses"
+    },{ 
+        title: "Arrays in JavaScript can be used to store ____",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answers: "All Of The Above"
+    },{
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal/Bash", "For Loops", "console.log"],
+        answers: "console.log"
+    },{
+        title: "When invoking a function, what set of punctuation must follow the variable?",
+        choices: ["Square Brackets", "Curly Brackets", "Quotation Marks", "Parenthesis"],
+        answers: "Parenthesis"
+    }
 ]
 
